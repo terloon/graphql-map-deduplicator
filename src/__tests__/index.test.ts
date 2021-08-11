124aa90d2429174ea2ed2e355470b9b748ab4857
Terence Ramos <terence.ramos@lacuna.ai>
2021-08-11 13:13:28 -0700

    Ran prettier and changed to not use semi.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,63 +1,62 @@
 import { mapit, unmapit } from '../index'
 
-
 test('mapit', () => {
-    const thing = {
-        __typename: 'thing',
+  const thing = {
+    __typename: 'thing',
+    id: 'a',
+    data: [
+      {
+        __typename: 'foo',
         id: 'a',
-        data: [
-            {
-                __typename: 'foo',
-                id: 'a',
-                propertyA: 123,
-                propertyB: 'hello',
-                bar: {
-                    __typename: 'bar',
-                    id: 'a',
-                    propertyA: 1,
-                    propertyB: 2,
-                    propertyC: 3,
-                    propertyD: 4
-                }                        
-            },
-            {
-                __typename: 'foo',
-                id: 'b',
-                propertyA: 234,
-                propertyB: 'world',
-                bar: {
-                    __typename: 'bar',
-                    id: 'a',
-                    propertyA: 1,
-                    propertyB: 2,
-                    propertyC: 3,
-                    propertyD: 4
-                },
-            },
-            {
-                __typename: 'foo',
-                id: 'c',
-                propertyA: 456,
-                propertyB: '!',
-                bar: {
-                    __typename: 'bar',
-                    id: 'a',
-                    propertyA: 1,
-                    propertyB: 2,
-                    propertyC: 3,
-                    propertyD: 4
-                },
-            }
-        ]        
-    }
+        propertyA: 123,
+        propertyB: 'hello',
+        bar: {
+          __typename: 'bar',
+          id: 'a',
+          propertyA: 1,
+          propertyB: 2,
+          propertyC: 3,
+          propertyD: 4,
+        },
+      },
+      {
+        __typename: 'foo',
+        id: 'b',
+        propertyA: 234,
+        propertyB: 'world',
+        bar: {
+          __typename: 'bar',
+          id: 'a',
+          propertyA: 1,
+          propertyB: 2,
+          propertyC: 3,
+          propertyD: 4,
+        },
+      },
+      {
+        __typename: 'foo',
+        id: 'c',
+        propertyA: 456,
+        propertyB: '!',
+        bar: {
+          __typename: 'bar',
+          id: 'a',
+          propertyA: 1,
+          propertyB: 2,
+          propertyC: 3,
+          propertyD: 4,
+        },
+      },
+    ],
+  }
 
-    const resultA = mapit(thing)
-    expect(resultA).toBeDefined()
-    const jsonA = JSON.stringify(resultA)
+  const resultA = mapit(thing)
+  expect(resultA).toBeDefined()
+  const jsonA = JSON.stringify(resultA)
 
-    const resultB = unmapit(resultA)
-    expect(resultB).toBeDefined()
-    const jsonB = JSON.stringify(resultB)
+  const resultB = unmapit(resultA)
+  expect(resultB).toBeDefined()
+  const jsonB = JSON.stringify(resultB)
 
-    expect(resultB).toEqual(thing)
+  expect(resultB).toEqual(thing)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 // import * from './mapit'
-export { default as mapit} from './mapit'
-export { default as unmapit} from './unmapit'
+export { default as mapit } from './mapit'
+export { default as unmapit } from './unmapit'
